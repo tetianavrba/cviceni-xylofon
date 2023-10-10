da91f1d887332501bd3cf507eb799af52d363328
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-10-10 15:16:42 +0200

    JS

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 console.log('Funguju')
+const nadpis = document.querySelector('h1')
+const kameny = document.querySelectorAll('.kamen')
+
+const prehrajTonNaKameni = (kamen) => {
+	const ton = kamen.textContent
+	nadpis.textContent = ton
+
+	const zvuk = new Audio(`tony/${ton}.mp3`)
+	zvuk.play()
+}
+
+const naStisknuti = (udalost) => {
+	prehrajTonNaKameni(udalost.target)
+}
+
+kameny.forEach((kamen) => {
+	kamen.addEventListener('click', naStisknuti)
+})
+
+// Bonus
+const naKlavesu = (udalost) => {
+	const { code } = udalost
+	if (code.startsWith('Digit')) {
+		const cislo = parseInt(code.substring(5))
+		if (cislo >= 1 && cislo <= kameny.length) {
+			prehrajTonNaKameni(kameny[cislo - 1])
+		}
+	}
+}
+
+document.addEventListener('keydown', naKlavesu)
